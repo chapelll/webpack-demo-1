9cd0331c3aca197b2efa7af8795eadc6ceb07147
chapelll <214426857@qq.com>
2020-05-19 13:22:06 +0800

    第二次提交

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,26 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: '[name].[contenthash].js'
-    }
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.[contenthash].js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: '配置中的title',
+            template: 'src/assets/index.html'
+        })
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.css$/,
+            use: [ 'style-loader', 'css-loader' ]
+          }
+        ]
+      }
 };
+
